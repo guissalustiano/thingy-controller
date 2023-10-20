cbc8b207112544e179b6ae7810c4661631c2c92d
Guilherme S. Salustiano <guissalustiano@gmail.com>
2023-10-20 21:40:27 +0200

    use embassy

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
-#![no_main]
 #![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
 
-use embedded_hal::digital::v2::OutputPin;
-use nrf52832_hal as hal;
-use nrf52832_hal::gpio::Level;
-use rtt_target::{rprintln, rtt_init_print};
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
 
-#[panic_handler] // panicking behavior
-fn panic(_: &core::panic::PanicInfo) -> ! {
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    let mut led = Output::new(p.P0_17, Level::Low, OutputDrive::Standard);
+
     loop {
-        cortex_m::asm::bkpt();
-    }
-}
-
-#[cortex_m_rt::entry]
-fn main() -> ! {
-    rtt_init_print!();
-    let p = hal::pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0);
-    let mut led = port0.p0_17.into_push_pull_output(Level::Low);
-
-    rprintln!("Blinky button demo starting");
-    loop {
-        led.set_high().unwrap();
-        cortex_m::asm::delay(1000000);
-        rprintln!("Set LED high");
-
-        led.set_low().unwrap();
-        cortex_m::asm::delay(1000000);
-        rprintln!("Set LED low");
+        led.set_high();
+        Timer::after_millis(300).await;
+        led.set_low();
+        Timer::after_millis(300).await;
     }
 }
