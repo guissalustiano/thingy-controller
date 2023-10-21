6077a597f3c8a998aebf4ac62c9d15b37fad56e5
Guilherme S. Salustiano <guissalustiano@gmail.com>
2023-10-21 16:47:18 +0200

    reafactor with no help alloc

---
+++ src/ble.rs
@@ -0,0 +1,89 @@
+use defmt::*;
+use embassy_executor::Spawner;
+use nrf_softdevice::ble::peripheral::AdvertiseError;
+use nrf_softdevice::{raw, Softdevice};
+use nrf_softdevice::ble::{peripheral, Connection};
+
+use core::mem;
+
+#[nrf_softdevice::gatt_service(uuid = "180f")]
+pub struct BatteryService {
+    #[characteristic(uuid = "2a19", read, notify)]
+    pub battery_level: i16,
+}
+
+#[nrf_softdevice::gatt_server]
+pub struct Server {
+    pub bas: BatteryService,
+}
+
+#[embassy_executor::task]
+async fn softdevice_task(sd: &'static Softdevice) -> ! {
+    sd.run().await
+}
+
+pub fn softdevice_setup<'a, const N: usize>(spawner: &'a Spawner, device_name: &[u8; N]) -> (&'a Softdevice, Server) {
+    let config = nrf_softdevice::Config {
+        clock: Some(raw::nrf_clock_lf_cfg_t {
+            source: raw::NRF_CLOCK_LF_SRC_RC as u8,
+            rc_ctiv: 16,
+            rc_temp_ctiv: 2,
+            accuracy: raw::NRF_CLOCK_LF_ACCURACY_500_PPM as u8,
+        }),
+        conn_gap: Some(raw::ble_gap_conn_cfg_t {
+            conn_count: 1,
+            event_length: 24,
+        }),
+        conn_gatt: Some(raw::ble_gatt_conn_cfg_t { att_mtu: 256 }),
+        gatts_attr_tab_size: Some(raw::ble_gatts_cfg_attr_tab_size_t {
+            attr_tab_size: raw::BLE_GATTS_ATTR_TAB_SIZE_DEFAULT.into(),
+        }),
+        gap_role_count: Some(raw::ble_gap_cfg_role_count_t {
+            adv_set_count: raw::BLE_GAP_ADV_SET_COUNT_DEFAULT as u8,
+            periph_role_count: raw::BLE_GAP_ROLE_COUNT_PERIPH_DEFAULT as u8,
+            central_role_count: 0,
+            central_sec_count: 0,
+            _bitfield_1: raw::ble_gap_cfg_role_count_t::new_bitfield_1(0),
+        }),
+        gap_device_name: Some(raw::ble_gap_cfg_device_name_t {
+            p_value: device_name as *const u8 as _,
+            current_len: N as u16,
+            max_len: N as u16,
+            write_perm: unsafe { mem::zeroed() },
+            _bitfield_1: raw::ble_gap_cfg_device_name_t::new_bitfield_1(raw::BLE_GATTS_VLOC_STACK as u8),
+        }),
+        ..Default::default()
+    };
+
+    let sd = Softdevice::enable(&config);
+    let server = unwrap!(Server::new(sd));
+
+    unwrap!(spawner.spawn(softdevice_task(sd)));
+
+    return (sd, server);
+}
+
+// https://docs.silabs.com/bluetooth/4.0/general/adv-and-scanning/bluetooth-adv-data-basics
+pub async fn advertise_connectable<const N: usize>(
+    sd: &Softdevice,
+    device_name: &[u8; N],
+) -> Result<Connection, AdvertiseError> 
+where
+   [(); N +9]:
+{
+    let adv_data = &mut [0; N + 9];
+
+    adv_data[..9].copy_from_slice(&[
+        0x02, 0x01, raw::BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE as u8,
+        0x03, 0x03, 0x09, 0x18,
+        (device_name.len() + 1) as u8, 0x09, 
+    ]);
+    adv_data[9..].copy_from_slice(device_name);
+
+    let config = peripheral::Config::default();
+    let scan_data = &[0x03, 0x03, 0x09, 0x18];
+    let adv = peripheral::ConnectableAdvertisement::ScannableUndirected { adv_data, scan_data };
+     peripheral::advertise_connectable(sd, adv, &config).await
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
 
-use core::mem;
+mod ble;
+
+use ble::{softdevice_setup, advertise_connectable, Server, ServerEvent, BatteryServiceEvent};
+
 
 use defmt::*;
 use embassy_executor::Spawner;
@@ -15,92 +20,14 @@ use embassy_time::{Duration, Timer};
 
 use futures::future::{select, Either};
 use futures::pin_mut;
-use nrf_softdevice::ble::{gatt_server, peripheral, Connection};
-use nrf_softdevice::{raw, Softdevice};
+use nrf_softdevice::ble::{gatt_server, Connection};
 
 use {defmt_rtt as _, panic_probe as _};
-
-/*
-#[embassy_executor::task(pool_size = 4)]
-async fn button_task(
-    n: usize,
-    mut btn: Input<'static, AnyPin>,
-    mut led: Output<'static, AnyPin>,
-) {
-    loop {
-        btn.wait_for_low().await;
-        info!("Button {:?} pressed!", n);
-        led.set_low();
-        info!("Turning on LED {:?}!", n);
-
-        btn.wait_for_high().await;
-        info!("Button {:?} released!", n);
-        led.set_high();
-        info!("Turning off LED {:?}!", n);
-    }
-}
-
-//#[embassy_executor::main]
-#[allow(dead_code)]
-async fn main_old(spawner: Spawner) {
-    let p = embassy_nrf::init(Default::default());
-    info!("Starting!");
-
-    let btn1 = Input::new(p.P0_13.degrade(), Pull::Up);
-    let btn2 = Input::new(p.P0_14.degrade(), Pull::Up);
-    let btn3 = Input::new(p.P0_15.degrade(), Pull::Up);
-    let btn4 = Input::new(p.P0_16.degrade(), Pull::Up);
-
-    let led1 = Output::new(p.P0_17.degrade(), Level::High, OutputDrive::Standard);
-    let led2 = Output::new(p.P0_18.degrade(), Level::High, OutputDrive::Standard);
-    let led3 = Output::new(p.P0_19.degrade(), Level::High, OutputDrive::Standard);
-    let led4 = Output::new(p.P0_20.degrade(), Level::High, OutputDrive::Standard);
-
-    unwrap!(spawner.spawn(button_task(1, btn1, led1)));
-    unwrap!(spawner.spawn(button_task(2, btn2, led2)));
-    unwrap!(spawner.spawn(button_task(3, btn3, led3)));
-    unwrap!(spawner.spawn(button_task(4, btn4, led4)));
-}
-*/
 
 bind_interrupts!(struct Irqs {
     SAADC => saadc::InterruptHandler;
 });
 
-// https://stackoverflow.com/a/77030892
-const fn eval<const N: usize>(data: &[&[u8]]) -> [u8; N] {
-    let mut result = [0; N];
-    
-    let mut i = 0;
-    let mut result_i = 0;
-    while i < data.len() {
-        let mut j = 0;
-        while j < data[i].len() {
-            result[result_i] = data[i][j];
-            result_i += 1;
-            j += 1;
-        }
-        i += 1;
-    }
-    
-    result
-}
-
-const fn count_len(arr: &[&[u8]]) -> usize {
-    let mut result = 0;
-    let mut i = 0;
-    while i < arr.len() {
-        result += arr[i].len();
-        i += 1;
-    }
-    result
-}
-
-macro_rules! declare_const {
-    ( const $const_name:ident = [ $($data:tt)* ] ) => {
-        const $const_name: [u8; count_len(&[ $($data)* ])] = eval(&[ $($data)* ]);
-    };
-}
 
 /// Initializes the SAADC peripheral in single-ended mode on the given pin.
 fn init_adc(adc_pin: AnyInput, adc: SAADC) -> Saadc<'static, 1> {
@@ -132,69 +59,13 @@ async fn notify_adc_value<'a>(saadc: &'a mut Saadc<'_, 1>, server: &'a Server, c
     }
 }
 
-#[embassy_executor::task]
-async fn softdevice_task(sd: &'static Softdevice) -> ! {
-    sd.run().await
-}
-
-#[nrf_softdevice::gatt_service(uuid = "180f")]
-struct BatteryService {
-    #[characteristic(uuid = "2a19", read, notify)]
-    battery_level: i16,
-}
-
-#[nrf_softdevice::gatt_server]
-struct Server {
-    bas: BatteryService,
-}
-
-const DEVICE_NAME: &'static [u8; 17] = b"Thingy Controller";
-
-fn softdevice_setup(spawner: &Spawner) -> (&Softdevice, Server) {
-
-    let config = nrf_softdevice::Config {
-        clock: Some(raw::nrf_clock_lf_cfg_t {
-            source: raw::NRF_CLOCK_LF_SRC_RC as u8,
-            rc_ctiv: 16,
-            rc_temp_ctiv: 2,
-            accuracy: raw::NRF_CLOCK_LF_ACCURACY_500_PPM as u8,
-        }),
-        conn_gap: Some(raw::ble_gap_conn_cfg_t {
-            conn_count: 1,
-            event_length: 24,
-        }),
-        conn_gatt: Some(raw::ble_gatt_conn_cfg_t { att_mtu: 256 }),
-        gatts_attr_tab_size: Some(raw::ble_gatts_cfg_attr_tab_size_t {
-            attr_tab_size: raw::BLE_GATTS_ATTR_TAB_SIZE_DEFAULT.into(),
-        }),
-        gap_role_count: Some(raw::ble_gap_cfg_role_count_t {
-            adv_set_count: raw::BLE_GAP_ADV_SET_COUNT_DEFAULT as u8,
-            periph_role_count: raw::BLE_GAP_ROLE_COUNT_PERIPH_DEFAULT as u8,
-            central_role_count: 0,
-            central_sec_count: 0,
-            _bitfield_1: raw::ble_gap_cfg_role_count_t::new_bitfield_1(0),
-        }),
-        gap_device_name: Some(raw::ble_gap_cfg_device_name_t {
-            p_value: DEVICE_NAME as *const u8 as _,
-            current_len: DEVICE_NAME.len() as u16,
-            max_len: DEVICE_NAME.len() as u16,
-            write_perm: unsafe { mem::zeroed() },
-            _bitfield_1: raw::ble_gap_cfg_device_name_t::new_bitfield_1(raw::BLE_GATTS_VLOC_STACK as u8),
-        }),
-        ..Default::default()
-    };
-
-    let sd = Softdevice::enable(&config);
-    let server = unwrap!(Server::new(sd));
-
-    unwrap!(spawner.spawn(softdevice_task(sd)));
-
-    return (sd, server);
-}
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     info!("Hello World!");
+
+    const DEVICE_NAME: &'static [u8; 18] = b"Thingy Wii Control";
+
 
     // First we get the peripherals access crate.
     let mut config = embassy_nrf::config::Config::default();
@@ -208,36 +79,10 @@ async fn main(spawner: Spawner) {
     // Indicated: wait for ADC calibration.
     saadc.calibrate().await;
 
-    let (sd, server) = softdevice_setup(&spawner);
-
-    const BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE: u8  = raw::BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE as u8;
-    const PAYLOAD_SIZE: u8 = (DEVICE_NAME.len() + 1) as u8;
-
-    #[rustfmt::skip]
-    declare_const!(const ADV_DATA = [
-        &[0x02, 0x01, BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE],
-        &[0x03, 0x03, 0x09, 0x18],
-        &[PAYLOAD_SIZE, 0x09], DEVICE_NAME,
-    ]);
-
-    #[rustfmt::skip]
-    const SCAN_DATA: [u8; 4] = [0x03, 0x03, 0x09, 0x18];
-
-    #[rustfmt::skip]
-    let _adv_data = &[
-        0x02, 0x01, raw::BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE as u8,
-        0x03, 0x03, 0x09, 0x18,
-        0x0a, 0x09, b'H', b'e', b'l', b'l', b'o', b'R', b'u', b's', b't',
-    ];
+    let (sd, server) = softdevice_setup(&spawner, &DEVICE_NAME);
 
     loop {
-        let config = peripheral::Config::default();
-
-        let adv = peripheral::ConnectableAdvertisement::ScannableUndirected { 
-            adv_data: &ADV_DATA, 
-            scan_data: &SCAN_DATA,
-        };
-        let conn = unwrap!(peripheral::advertise_connectable(sd, adv, &config).await);
+        let conn = unwrap!(advertise_connectable(sd, &DEVICE_NAME).await);
         info!("advertising done! I have a connection.");
 
         // We have a GATT connection. Now we will create two futures:
