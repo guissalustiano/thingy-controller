94fb7955fe24b1bf33068dcde88163c5023f886c
Guilherme S. Salustiano <guissalustiano@gmail.com>
2023-10-21 02:39:29 +0200

    tring acess i2c port extension

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,44 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_time::Timer;
+use embassy_nrf::twim::{self, Twim};
+use embassy_nrf::{bind_interrupts, peripherals};
 use {defmt_rtt as _, panic_probe as _};
+
+// sx1509
+const SX1509_ADDRESS: u8 = 0x3e;
+
+bind_interrupts!(struct Irqs {
+    SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => twim::InterruptHandler<peripherals::TWISPI0>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut led = Output::new(p.P0_17, Level::Low, OutputDrive::Standard);
+    info!("Initializing TWI...");
+    let config = twim::Config::default();
+    let mut twi = Twim::new(p.TWISPI0, Irqs, p.P0_07, p.P0_08, config);
 
-    loop {
-        led.set_high();
-        Timer::after_millis(300).await;
-        led.set_low();
-        Timer::after_millis(300).await;
-    }
+
+    let mask: u8 = 0xFF;
+
+    info!("Applying reset");
+    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x7D, 0x12]));
+    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x7D, 0x34]));
+
+    info!("Setting back direction");
+    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x0F, 0x00]));
+    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x0E, 0x00]));
+
+
+    info!("Setting up LED");
+    // Write Reg PullUp B
+    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x07, mask]));
+    //
+    // Write Reg PullUp A
+    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x06, mask]));
+
+    info!("Shine!");
 }
