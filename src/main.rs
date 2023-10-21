b75a02d2fe79a42379a20e099a8f98ce926a95d7
Guilherme S. Salustiano <guissalustiano@gmail.com>
2023-10-21 03:09:06 +0200

    try sx1509

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,6 @@ use embassy_nrf::twim::{self, Twim};
 use embassy_nrf::{bind_interrupts, peripherals};
 use {defmt_rtt as _, panic_probe as _};
 
-// sx1509
-const SX1509_ADDRESS: u8 = 0x3e;
-
 bind_interrupts!(struct Irqs {
     SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => twim::InterruptHandler<peripherals::TWISPI0>;
 });
@@ -22,24 +19,16 @@ async fn main(_spawner: Spawner) {
     let config = twim::Config::default();
     let mut twi = Twim::new(p.TWISPI0, Irqs, p.P0_07, p.P0_08, config);
 
-
-    let mask: u8 = 0xFF;
-
+    let mut expander = sx1509::Sx1509::new(&mut twi, sx1509::DEFAULT_ADDRESS);
     info!("Applying reset");
-    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x7D, 0x12]));
-    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x7D, 0x34]));
+    unwrap!(expander.borrow(&mut twi).software_reset());
 
     info!("Setting back direction");
-    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x0F, 0x00]));
-    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x0E, 0x00]));
+    unwrap!(expander.borrow(&mut twi).set_bank_a_direction(0));
 
+    info!("Reading pins");
+    // read the pins from bank a
+    let pins = unwrap!(expander.borrow(&mut twi).get_bank_a_data());
 
-    info!("Setting up LED");
-    // Write Reg PullUp B
-    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x07, mask]));
-    //
-    // Write Reg PullUp A
-    unwrap!(twi.blocking_write(SX1509_ADDRESS, &[0x06, mask]));
-
-    info!("Shine!");
+    info!("pins: {=u8}", pins);
 }
